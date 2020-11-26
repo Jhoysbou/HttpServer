7eee62cc3ba72f5f13bdf5e70163a9e415e7f0dc
jhoysbou <neeroll21@gmail.com>
2020-11-26 21:12:27 +0300

    params

--- HttpServer/main.cpp
+++ HttpServer/main.cpp
@@ -90,56 +90,53 @@ double* parseBody(char requestBuffer[]) {
 
 #define PORT 8080
 int main(int argc, char const *argv[]) {
-    char data[] = "GET / HTTP/1.1\r\nHost: localhost:8080\r\nContent-Type: application/json\r\nContent-Length: 77\r\n\r\n{\r\n    \"steps\": 100,\r\n    \"l_1\": 1,\r\n    \"l_2\": 1,\r\n    \"m_1\": 1,\r\n    \"m_2\": 1\r\n}";
-    double* params = parseBody(data);
-    std::string simData = solve(params);
-    std::cout << simData << std::endl;
-//    int server_fd, new_socket; long valread;
-//    struct sockaddr_in address;
-//    int addrlen = sizeof(address);
-//
-//
-//
-//    // Creating socket file descriptor
-//    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-//        perror("In socket");
-//        exit(EXIT_FAILURE);
-//    }
-//
-//
-//    address.sin_family = AF_INET;
-//    address.sin_addr.s_addr = INADDR_ANY;
-//    address.sin_port = htons( PORT );
-//
-//    memset(address.sin_zero, '\0', sizeof address.sin_zero);
-//
-//
-//    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-//        perror("In bind");
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    if (listen(server_fd, 10) < 0) {
-//        perror("In listen");
-//        exit(EXIT_FAILURE);
-//    }
-//    while(1) {
-//        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
-//        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-//            perror("In accept");
-//            exit(EXIT_FAILURE);
-//        }
-//        std::string simData = solve(100);
-//        std::string response = ::response(simData);
-//
-//        char buffer[30000] = {0};
-//        valread = read( new_socket , buffer, 30000);
-//        parseBody(buffer);
-////        printf("%s\n",buffer );
-//        write(new_socket , response.c_str(), response.length());
-//        printf("------------------ Data sent -------------------\n");
-//        close(new_socket);
-//    }
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1) {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+       
+
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        double* params = parseBody(buffer);
+        std::string simData = solve(params);
+        std::string response = ::response(simData);
+//        printf("%s\n",buffer );
+        write(new_socket , response.c_str(), response.length());
+        printf("------------------ Data sent -------------------\n");
+        close(new_socket);
+    }
     return 0;
 }
 
